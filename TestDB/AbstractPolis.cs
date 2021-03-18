9788710d8c3373989b4c38024e42614f3ec9fbfc
Alex <pichuev.alex@yandex.ru>
2021-03-18 18:54:05 +0700

    Add project files.

---
+++ TestDB/AbstractPolis.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestDB
+{
+    public class AbstractPolis
+    {
+        public readonly DateTime startDate;
+        public readonly DateTime closeDate;
+        public readonly string ID;
+        public readonly string ownerName;
+        public readonly string ownerSurname;
+        public readonly DateTime birthDate;
+        string insuredObjectName;
+        string insuredObjectType;
+        string status;
+        DateTime lastUpdate;
+
+        public AbstractPolis(DateTime startDate, DateTime closeDate, string ID, string ownerName, string ownerSurname, DateTime birthDate, string insuredObjectName, string insuredObjectType, string status, DateTime lastUpdate)
+        {
+            this.startDate = startDate;
+            this.closeDate = startDate;
+        }
+    }
+}
---
+++ TestDB/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime startDate = new DateTime(2015, 5, 20);
+            DateTime closeDate = new DateTime(2015, 7, 20);
+            //Console.WriteLine(startDate.ToString("MMM"));
+        }
+    }
+}
