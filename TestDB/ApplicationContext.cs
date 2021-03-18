028f6dfbe5c786fb49359006cd53efa4a6871046
Alex <pichuev.alex@yandex.ru>
2021-03-18 20:48:56 +0700

    Create DB

---
+++ TestDB/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestDB
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<AbstractPolis> polises { get; set; }
+
+    }
+}
