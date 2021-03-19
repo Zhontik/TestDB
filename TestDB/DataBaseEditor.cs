04a4dd5fde232f081d1cfc034f9d8bef4c595049
Alex <pichuev.alex@yandex.ru>
2021-03-19 21:56:31 +0700

    in progress

---
+++ TestDB/DataBaseEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestDB
+{
+    class DataBaseEditor
+    {
+        ApplicationContext datab;
+
+        public int AddNewPolicy(DateTime closeDate, string ownerName, string ownerSurname, DateTime birthDate, string insuredObjectName, string TypeinsuredObjectType)
+        {
+
+
+
+            AbstractPolicy pol = new AbstractPolicy(DateTime.Now, DateTime.Now.AddDays(31), ++num, "Alex", "Pichuev", new DateTime(1997, 8, 28), "MyLife", "Life");
+            return 0;
+        }
+
+        /*public List<AbstractPolicy> get()
+        {
+
+            return 0;
+        }*/
+    }
+}
